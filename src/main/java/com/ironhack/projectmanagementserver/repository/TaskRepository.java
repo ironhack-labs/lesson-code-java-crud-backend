1500dfdea5983629a13e31cd57c0b03061c537b1
unknown <raymond.maroun@ironhack.com>
2022-10-11 17:31:27 +0300

    Updating repository

--- src/main/java/com/ironhack/projectmanagementserver/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ironhack.projectmanagementserver.repository;
-
-import com.ironhack.projectmanagementserver.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task,Long> {
-}
--- src/main/java/com/ironhack/projectmanagementserver/service/interfaces/TaskServiceInterface.java
+++
@@ -1,7 +0,0 @@
-package com.ironhack.projectmanagementserver.service.interfaces;
-
-import com.ironhack.projectmanagementserver.DTO.TaskDTO;
-
-public interface TaskServiceInterface {
-    void saveTask(TaskDTO task);
-}
